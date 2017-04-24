368c65a9be94d4ea82421e12c5d529326e44cdfd
Ben Peek <mschfmyhmsoap@oit117253.local>
2017-04-23 23:00:26 -0500

    Our real first commit.

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Ben Peek on 4/23/17.
+//  Copyright © 2017 Ben Peek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
